165854b86cdd358dc86c9875c7802f6cee0dbd13
José Guilherme <jose-guilherme93@hotmail.com>
2021-08-20 21:36:34 -0300

    Add files via upload
    first commit

---
+++ counter.js
@@ -0,0 +1,12 @@
+
+
+
+setInterval ( () =>{
+    var frutas = ['melancia', 'abacate', 'melão']
+
+for(var contador = 0; contador < frutas.length; contador++) 
+console.log(frutas[contador]) 
+if (counter < frutas.length) {
+    continue
+}
+},1000)
